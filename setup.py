aa9baeb68598d7c945fe58bf1fc4b41eeecf8454
havocesp <danielumpierrezdelrio@gmail.com>
2018-05-17 23:22:35 +0000

    New version set

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panance',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(exclude=['doc*', 'test*', 'venv*']),
     url='https://github.com/havocesp/panance',
     license='MIT',
